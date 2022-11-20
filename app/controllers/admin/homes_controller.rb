d663ac41709ffb4c4c38d51d68cd94892b7b7700
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-20 06:49:01 +0000

    [update]admin_homes_controller

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,9 @@ class Admin::HomesController < ApplicationController
   def top
     #@params = params[:id]
     #@order = Order.all
+
     #@search = Item.ransack(params[:q])
     #@items = @search.result
+    #↑検索機能
   end
 end
