7cf9aba0c1bb69be3a01ab7d2f280098874b0b1c
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-20 06:45:13 +0000

    [update]admin_customers_controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,40 @@
 class Admin::CustomersController < ApplicationController
   def index
+    #@customers = Customer.all
+
+    #@search = Item.ransack(params[:q])
+    #@items = @search.result
+    #↑検索機能
   end
 
   def show
+    #@customer = Customer.find(params[:id])
+
+    #@search = Item.ransack(params[:q])
+    #@items = @search.result
+    #↑検索機能
   end
 
   def edit
+    #@customer = Customer.find(params[:id])
+
+    #@search = Item.ransack(params[:q])
+    #@items = @search.result
+    #↑検索機能
   end
 
   def update
-  end
+    #@genres = Genre.all
+    #@customer = Customer.find(params[:id])
+    #@customerorders = Order.where(customer_id: @customer.id)
 
+    #@search = Item.ransack(params[:q])
+    #@items = @search.result
+    #↑検索機能
+  end
+   private
+
+  def customer_params
+    #params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :phone_number, :address, :is_deleted)
+  end
 end
