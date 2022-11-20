ec91c0d99c201bd28fc662ceea996fe4b32741f2
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-20 07:06:28 +0000

    [Add]モデルに税込の記述追加、サンプル画像の変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,15 @@ class Item < ApplicationRecord
   has_one_attached :item_image
 
   def get_item_image(width, height)
-  unless item_image.attached?
-    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-    item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-  item_image.variant(resize_to_limit: [width, height]).processed
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.png')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+      item_image.variant(resize_to_limit: [400, 300], "#ffffff", "Center").processed
   end
 
+  def tax_price
+    (price*1.1).floor
+  end
 
 end
