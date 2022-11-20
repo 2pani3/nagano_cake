03ea57ac5c616771c039cd7702019ce292caefe2
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-20 08:16:04 +0000

    [Save]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,16 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page])
+    # @items = Item.page(params[:page])
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
-  
+
   private
  def item_params
   params.require(:item).permit(:name,:item_image,:price)
  end
- 
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,19 @@
 class Item < ApplicationRecord
-  　#１つのジャンルに属している
+  　#1つのジャンルに属している
   belongs_to :genre
-    # カートに入った商品たちを持っている
+    #カートに入った商品たちを持っている
   has_many :cart_items, dependent: :destroy
-    # 注文詳細たちを持っている
+    #注文詳細たちを持っている
   has_many :order_details, dependent: :destroy
   has_one_attached :item_image
+
 
   def get_item_image(width, height)
     unless item_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.png')
       item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
     end
-      item_image.variant(resize_to_limit: [400, 300], "#ffffff", "Center").processed
+      item_image.variant(resize_to_limit: [400, 300]).processed
   end
 
   def tax_price
