90f54b16f7b03023cb39c0a8dcdc53a593f4edd9
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-21 07:30:40 +0000

    [Add]indexのコントローラ記述追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,10 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.all
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += cart_item.item.tax_price*cart_item.amount
+    end
   end
 
   def update
