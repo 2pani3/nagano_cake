a7d286b2a678dfa1392dd51c08db33d5a97d0cb1
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-21 03:29:30 +0000

    [edit]admin_customers_controllerの修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -35,6 +35,6 @@ class Admin::CustomersController < ApplicationController
    private
 
   def customer_params
-    #params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :phone_number, :address, :is_deleted)
+    #params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :phone_number, :address, :is_deleted)
   end
 end
