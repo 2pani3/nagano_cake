9509a967af91a3c687f5d3a1f6b262eefede81fb
EC2 Default User <ec2-user@ip-172-31-2-38.ap-northeast-1.compute.internal>
2022-11-21 02:46:13 +0000

    [Add]レイアウト修正途中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,10 +1,32 @@
-class Public::SessionsController < ApplicationController
-  def new
-  end
+# frozen_string_literal: true
 
-  def create
-  end
+class Public::SessionsController < Devise::SessionsController
+   before_action :configure_sign_in_params, only: [:create]
 
-  def destroy
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+
+  def after_sign_in_path_for(resource)
+    # flash[:notice] = "Welcome! You have signed up successfully."
+    root_path
   end
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
