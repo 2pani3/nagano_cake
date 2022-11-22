8ce0abcefce8c461189a7aac9efbc04bc212d51e
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-22 08:37:23 +0000

    [fix]admin_homes_controllerの編集とコメントアウト

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,8 @@
 class Admin::HomesController < ApplicationController
   def top
-    #@params = params[:id]
-    #@order = Order.all
+    @params = params[:id]
+    @order = Order.where(customer_id: @params)
+    @orders = Order.all
 
     #@search = Item.ransack(params[:q])
     #@items = @search.result
