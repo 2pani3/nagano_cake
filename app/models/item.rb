3405354eaf6bd875d34611ed9dc48e13f8430fa3
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2022-11-22 12:49:28 +0000

    [add]topページ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,23 @@
 class Item < ApplicationRecord
-  　#１つのジャンルに属している
+    #1つのジャンルに属している
   belongs_to :genre
-    # カートに入った商品たちを持っている
+    #カートに入った商品たちを持っている
   has_many :cart_items, dependent: :destroy
-    # 注文詳細たちを持っている
+    #注文詳細たちを持っている
   has_many :order_details, dependent: :destroy
   has_one_attached :item_image
 
+
   def get_item_image(width, height)
-  unless item_image.attached?
-    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-    item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-  item_image.variant(resize_to_limit: [width, height]).processed
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.png')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+      item_image.variant(resize_to_limit: [width, height]).processed
   end
 
+  def tax_price
+    (price*1.1).floor
+  end
 
 end
