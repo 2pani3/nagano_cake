2568072ef44058893ac170cd80f2ac3717901bde
EC2 Default User <ec2-user@ip-172-31-2-38.ap-northeast-1.compute.internal>
2022-11-22 09:06:13 +0000

    ［Add］退会処理一連の流れ終了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -21,6 +21,10 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdrawal
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path, notice: "退会処理を実行しました"
   end
 
 private
