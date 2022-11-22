0c623d0dcf91273ba22d633f8b9d303eda92790e
EC2 Default User <ec2-user@ip-172-31-2-38.ap-northeast-1.compute.internal>
2022-11-22 03:47:21 +0000

    ［Add］登録情報編集画面完了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,9 +4,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    # @customer = Customer.find(params[:id])
+    @customer = current_customer
+  if@customer.update(customer_params)
+    flash[:notice] = "更新が完了しました！"
+    redirect_to customer_path(@customer.id)
+  end
   end
 
   def quit
@@ -14,8 +21,8 @@ class Public::CustomersController < ApplicationController
 
   def withdrawal
   end
-  
-private  
+
+private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
   end
