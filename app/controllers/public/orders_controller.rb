01367160d17b036fcabd1b4619039de804e35807
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-23 10:45:23 +0000

    [Add]コントローラの記述追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,7 +5,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
+    @cart_items = current_customer.cart_items.all
     @order = Order.new(order_params)
+    @total 
     @shipping_fee = 800
 
     if params[:order][:select_address] == "0"
@@ -13,13 +15,16 @@ class Public::OrdersController < ApplicationController
       @order.shipping_address = current_customer.address
       @order.shipping_name = current_customer.name.full_name_nospace
 
-    elsif params[:order][:select_address_registar] == "1"
+    elsif params[:order][:select_address] == "1"
       @address = Address.find(params[:order][:address_id])
       @order.shipping_postcode = Address.postcode
       @order.shipping_address = Address.address
       @order.shipping_name = Address.name.full_name_nospace
 
-    elsif params[:order][:select_address_new] == "2"
+    elsif params[:order][:select_address] == "2"
+      @order.shipping_postcode = params[:order][:shipping_postcode]
+      @order.shipping_address = params[:order][:shipping_address]
+      @order.shipping_name = params[:order][:shipping_name]
       @order.save
 
     else
