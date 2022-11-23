7910edcdd3439941417eca44a0915aa0b5721d9f
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-23 07:50:44 +0000

    [Fix]admin_orders_controller記述修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,17 +2,17 @@ class Admin::OrdersController < ApplicationController
     
   def show
     @order = Order.find(params[:id])
-    @order_details= @order.order_details
+    @order_details = OrderDetail.find(params[:id])
   end
   
   def update
     @order = Order.find(params[:id])
-    @order_items = @order.order_items
+    @order_details = @order.order_details
      if @order.update(order_params)
-      if @order.status == "confirmation"
-       @order_items.update_all(making_status: :b)
+      if @order.status == "確認"
+       @order_details.update_all(making_status: :b)
       end
-       redirect_to request.referer
+       redirect_to admin_order_path(@order)
      end
   end
 
@@ -20,7 +20,7 @@ class Admin::OrdersController < ApplicationController
   private
   
   def order_params
-     params.require(:order).permit(:status)
+     params.require(:order).permit(:customer_id, :shipping_cost, :total_payment, :order_status)
   end
   
 
