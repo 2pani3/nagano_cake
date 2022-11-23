dbc3f8248cb3e97408912a7966b9b65198b390d5
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-23 10:05:22 +0000

    [Add]コントローラの記述追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,9 +6,26 @@ class Public::OrdersController < ApplicationController
 
   def check
     @order = Order.new(order_params)
-    
-    if params[:order][:address_option] == "0"
-      @order.
+    @shipping_fee = 800
+
+    if params[:order][:select_address] == "0"
+      @order.shipping_postcode = current_customer.postcode
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.name.full_name_nospace
+
+    elsif params[:order][:select_address_registar] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.shipping_postcode = Address.postcode
+      @order.shipping_address = Address.address
+      @order.shipping_name = Address.name.full_name_nospace
+
+    elsif params[:order][:select_address_new] == "2"
+      @order.save
+
+    else
+      render :new
+    end
+
   end
 
   def complete
@@ -22,7 +39,7 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-  
+
 private
   def order_params
     params.require(:order).permit(:name, :address, :postcode)
