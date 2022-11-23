7653961263b6057ce652ac57b0a443511dbcf273
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-23 03:13:05 +0000

    [update]admin_orders_controllerの記述の追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,23 +1,26 @@
 class Admin::OrdersController < ApplicationController
     
   def show
-    # @order = Order.find(params[:id])
-    # @order_details= @order.order_details
+    @order = Order.find(params[:id])
+    @order_details= @order.order_details
   end
   
-  def index
-    #@orders = Order.all
-  end
   def update
-    # @order= Order.find(params[:id])
-    # @order.update(order_params)
-    # redirect_to admin_order_path(@order)
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+     if @order.update(order_params)
+      if @order.status == "confirmation"
+       @order_items.update_all(making_status: :b)
+      end
+       redirect_to request.referer
+     end
   end
+
   
   private
   
   def order_params
-    #params.require(:order).permit(:customer_id, :shipping_cost, :total_payment, :order_status)
+     params.require(:order).permit(:status)
   end
   
 
