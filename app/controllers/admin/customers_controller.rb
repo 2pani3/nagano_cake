4edfdc7c235dde210ba80737f513e63f0a2a1d90
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-23 10:07:05 +0000

    [Fix]admin_customers_controllerの記述修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -29,7 +29,7 @@ class Admin::CustomersController < ApplicationController
      if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer)
      else
-      render edit_admin_customer_path(@customer)
+      redirect_to edit_admin_customer_path(@customer)
      end
     end 
     
