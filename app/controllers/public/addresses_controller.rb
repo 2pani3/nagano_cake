5f13e89d5888136e91f377c7c01442f67427193b
EC2 Default User <ec2-user@ip-172-31-2-38.ap-northeast-1.compute.internal>
2022-11-23 03:27:31 +0000

    ［Add］レイアウト編集途中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,23 @@
 class Public::AddressesController < ApplicationController
   def index
-  end
-
-  def edit
+    @customer = current_customer
   end
 
   def create
+    
+  end
+
+  def edit
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:postcode, :address, :phone_number)
   end
 end
