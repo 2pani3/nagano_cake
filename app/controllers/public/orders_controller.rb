58f0e375e309b9077f572754d8a3d057ed29fc36
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-24 08:28:28 +0000

    [Add]記述の変更

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,58 @@
+class Public::OrdersController < ApplicationController
+  def new
+    # newブランチはここだけ
+    @order = Order.new
+  end
+
+  def check
+    @cart_items = current_customer.cart_items.all
+    @order = Order.new(order_params)
+    @shipping_fee = 800
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += cart_item.item.tax_price*cart_item.amount
+    end
+
+    if params[:order][:select_address] == "0"
+      @order.shipping_postcode = current_customer.postcode
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.full_name_nospace
+
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.shipping_postcode = @address.postcode
+      @order.shipping_address = @address.address
+      @order.shipping_name = @address.name
+
+    elsif params[:order][:select_address] == "2"
+      unless params[:order][:select_address] == ""
+        
+      
+      @order.shipping_postcode = params[:order][:shipping_postcode]
+      @order.shipping_address = params[:order][:shipping_address]
+      @order.shipping_name = params[:order][:shipping_name]
+      @order.save
+      end
+    else
+      render :new
+    end
+
+  end
+
+  def complete
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+private
+  def order_params
+    params.require(:order).permit(:shipping_postcode, :shipping_address, :shipping_name, :payment_way, :total_payment, :status, :shipping_fee)
+  end
+end
---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order < ApplicationRecord
+    # 1人のcustomerに属している
+  belongs_to :customer
+    # 注文詳細たちを持っている
+  has_many :order_details, dependent: :destroy
+  # enimの記述
+  enum status: { weiting: 0, check: 1, making: 2, prepare: 3, complete: 4 }, _prefix: true
+  enum payment_way: { credit_card: 0, transfer: 1 }
+
+  # 要件外 新しいお届け先が空の場合
+  validates :shipping_postcode,:shipping_address,:shipping_name, presence: true
+
+  def tax_price
+    (price*1.1).floor
+  end
+end
