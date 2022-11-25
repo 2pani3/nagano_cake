5689f67813cdddfd61a355d499bb8c325e4fb093
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-25 10:00:03 +0000

    [Fix]サンプル画像修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
 
   def get_item_image(width, height)
     unless item_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.png')
+      file_path = Rails.root.join('app/assets/images/coming_soon.png')
       item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
     end
       item_image.variant(resize_to_limit: [width, height]).processed
