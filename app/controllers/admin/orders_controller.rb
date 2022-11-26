44333ead3631ca9f8818acc070465c1bb5ef6cab
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-26 12:14:30 +0000

    [Fix]注文ステータス、制作ステータス機能の修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,29 @@
 class Admin::OrdersController < ApplicationController
+    
   def show
+    @order = Order.find(params[:id])
+    @order_details= @order.order_details
+
+
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+     if @order.update(order_params)
+      @order_details.update_all(making_status: 1) if @order.status == "check"
+      
+     end
+       redirect_to admin_order_path(@order)
+     
   end
 
-  def update
+  
+  private
+  
+  def order_params
+     params.require(:order).permit(:shipping_postcode, :shipping_address, :shipping_name, :payment_way, :total_payment, :status, :shipping_fee )
   end
+  
 
 end
